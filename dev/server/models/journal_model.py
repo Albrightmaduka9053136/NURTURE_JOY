2e2b893f12143a406d3ac24686203e458c4909c6
Lesley Wanjiku <wanjiku.kamash19@gmail.com>
2026-03-12 12:21:18 -0400

    model to create the journal model to save the journal entries in the database

---
+++ dev/server/models/journal_model.py
@@ -0,0 +1,19 @@
+from database.db import db
+from datetime import datetime
+
+class JournalEntry(db.Model):
+    __tablename__ = "journal_entries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    prompt = db.Column(db.String(255))
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "prompt": self.prompt,
+            "content": self.content,
+            "created_at": self.created_at.isoformat()
+        }
