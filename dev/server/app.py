0fde7eeceb119372c389928ecda4b813ecab921d
Lesley Wanjiku <wanjiku.kamash19@gmail.com>
2026-03-12 12:31:32 -0400

    added safety model logic to help with routes

--- dev/server/app.py
+++ dev/server/app.py
@@ -5,10 +5,28 @@ from config import Config
 from database.db import db
 from models.user_model import User
 from routes.auth_routes import auth_bp
+from routes.chat_routes import chat_bp
+from routes.mood_routes import mood_bp
+from routes.journal_routes import journal_bp
+import joblib
+import json
 
 
 
 app = Flask(__name__)
+
+app.config["SAFETY_MODEL"] = joblib.load("ml_models/nurturejoy_safety_model_v2.joblib")
+app.config["STAGE1_VECTORIZER"] = joblib.load("ml_models/stage1_tfidf.joblib")
+app.config["EMOTION_MODEL"] = joblib.load("ml_models/nurturejoy_emotion_model_v2.joblib")
+app.config["STAGE2_VECTORIZER"] = joblib.load("ml_models/stage2_tfidf.joblib")
+
+with open("ml_models/chatbot_templates.json") as f:
+    app.config["CHAT_TEMPLATES"] = json.load(f)
+    
+    
+app.register_blueprint(chat_bp)   # Register chat routes blueprint after defining it in chat_routes.py
+app.register_blueprint(mood_bp)   # Register mood routes blueprint after defining it in mood_routes.py
+app.register_blueprint(journal_bp)   # Register journal routes blueprint after defining it in journal_routes.py
 
 app.secret_key = os.urandom(24)
 # app.config["SESSION_KEY"] = "eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImphbmUifQ.aZQCIw.iuLocljSzNr9SaNzpx6jqWREahY"
@@ -23,12 +41,6 @@ app.register_blueprint(auth_bp)
 
 with app.app_context():
     db.create_all()
-
-
-
-
-
-
 
 @app.route("/")
 def home():
