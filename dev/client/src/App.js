82ce5d2dab4afce4a064a8278bb69a51702bba18
Lesley Wanjiku <wanjiku.kamash19@gmail.com>
2026-04-10 01:55:51 -0400

    new pages added

--- dev/client/src/App.js
+++ dev/client/src/App.js
@@ -1,11 +1,12 @@
 import {Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/components/Login";
+import Signup from "./pages/components/SignUp";
+import Dashboard from "./pages/components/Dasshboard";
 import Landing from "./pages/Landing";
 
-import SessionHistory from "./pages/SessionHistory";
+// import SessionHistory from "./pages/SessionHistory";
+import ChatHistory from "./pages/chatbot/ChatHistory";
 
 /* import Community from "./pages/Community"; */ // Placeholder for future community page
 import BreathingBox from "./pages/resources/BreathingBox";
@@ -13,6 +14,8 @@ import Grounding54321 from "./pages/resources/Grounding54321";
 import CareProviders from "./pages/resources/CareProviders";
 import Journal from "./pages/resources/JournalPrompts";
 import UrgentSupport from "./pages/resources/UrgentSupport";
+import Resources from "./pages/Resources";
+import UserProfile from "./pages/components/UserProfile";
 
 
 function App() {
@@ -23,8 +26,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup/>} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sessions" element={<SessionHistory />} />
+        <Route path="/sessions" element={<ChatHistory />} />
+        <Route path="/profile" element={<UserProfile />} />
         {/* <Route path="/community" element={<Community />} /> */}
+        <Route path="/resources" element={<Resources/>} />
         <Route path="/resources/breathing-box" element={<BreathingBox />} />
         <Route path="/resources/grounding-54321" element={<Grounding54321 />} />
         <Route path="/resources/care-providers" element={<CareProviders />} />
